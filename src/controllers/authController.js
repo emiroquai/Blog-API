89985039bc4d71d6bebe81a021114a5dd4debb2b
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-09-25 16:38:45 +0200

    Add logic and endpoints for user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,24 @@
-const { prisma } = require("../config/prisma");
+const authService = require("../services/authService");
+const asyncHandler = require("express-async-handler");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, password } = req.body;
+  const user = await userService.createUser(name, password);
+  res.status(201).json(user);
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { name, password } = req.body;
+
+  if (!name || !password) {
+    return res.status(400).json({ message: "Name and password are required" });
+  }
+
+  const { user, token } = await authService.loginUser(name, password);
+  res.status(200).json({ message: "Login successful", user, token });
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,40 @@
-const { prisma } = require("../config/prisma");
+const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (name, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return await prisma.user.create({
+    data: {
+      name,
+      password: hashedPassword,
+    },
+  });
+};
+
+const loginUser = async (name, password) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      name,
+    },
+  });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const token = jwt.sign(
+      { id: user.id, name: user.name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return { user, token };
+  } else {
+    throw new Error("Invalid credentials");
+  }
+};
+
+module.exports = {
+  loginUser,
+  registerUser,
+};
